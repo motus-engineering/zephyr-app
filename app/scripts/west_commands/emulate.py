5e1fbfe97e3193d9252b7fcc8a6e27a1e85cf461
MotusDesignSallySchupbach <sally.schupbach@motusdesign.ca>
2024-01-15 18:58:47 +0000

    Add session logging to .resc

--- app/scripts/west_commands/emulate.py
+++ app/scripts/west_commands/emulate.py
@@ -12,6 +12,12 @@ from devicetree import dtlib
 from dts2repl import dts2repl # Create repl files for renode from device tree files
 from argparse import Namespace
 
+import glob
+
+root_dir = '/workspaces/zephyr-app/'
+build_dir = root_dir + 'build/'
+bin_dir = build_dir + 'zephyr/'
+
 class Emulate(WestCommand):
 
     def __init__(self):
@@ -37,23 +43,49 @@ class Emulate(WestCommand):
         parser.add_argument('-i', '--instances', help='Number board instances to emulate')
 
         return parser # gets stored as self.parser
+    
+    def _load_platform(self):
+        #Parse the zephyr CMakeCache.txt output to find the platform name and path
+        #CMakeCache.txt has line entries of the form <PROPERTY NAME>:<PROPERTY TYPE>=<PROPERTY VALUE>
+        platform_name = ''
+        platform_path = ''
+        platform_file = ''
+        with open(build_dir + 'CMakeCache.txt', 'r') as cmake_cache_file:
+            cmake_cache = cmake_cache_file.read()
+            cmake_cache = cmake_cache.splitlines()
+            for line in cmake_cache:
+                if line.startswith('BOARD:STRING'):
+                    platform_name = line.split('=', 1)[1]
+                if line.startswith('BOARD_DIR:PATH'):
+                    platform_path = line.split('=', 1)[1]
+                if platform_name and platform_path:
+                    log.inf('Found ' + platform_name + ' from ' + platform_path)
+                    break #Found both. Stop looking
 
+        #Look for a platform description in the board's support files
+        #If none exists, we can generate a basic one from the flattened device tree
+        platform_files = glob.glob(platform_path + '/support/*.repl')
+        if platform_files:
+            platform_file = platform_files[0]
+            log.inf('Found ' + platform_file)
+        else:
+            platform_file = bin_dir + 'platform.repl'
+            with open(platform_file, 'w') as repl_file:
+                repl_file.write(dts2repl.generate(Namespace(filename=(bin_dir + 'zephyr.dts'))))
+                log.inf('Generated ' + platform_file + ' from device tree')
+
+        return platform_name, platform_file
+        
     def do_run(self, args, unknown_args):
         # This gets called when the user runs the command, e.g.:        
         #   $ west emulate <action>
 
-        root_dir = '/workspaces/'
-        output_dir = root_dir + 'zephyr-app/build/zephyr/'
-
         log.inf('Preparing platform')
-
-        # Generate platform description file (.repl) from flattened device tree
-        with open(output_dir + 'platform.repl', "w") as repl_file:
-            repl_file.write(dts2repl.generate(Namespace(filename=(output_dir + 'zephyr.dts'))))
-
+        platform_name, platform_file = self._load_platform()
+        
         # Get assigned console port from flattened device tree
         console_node_name = ''
-        board_dt = dtlib.DT(filename=(output_dir + 'zephyr.dts'))
+        board_dt = dtlib.DT(filename=(bin_dir + 'zephyr.dts'))
         dt_chosen = board_dt.get_node('/chosen')
         if dt_chosen.props.get('zephyr,console'):
             console_node = dt_chosen.props.get('zephyr,console').to_path()
@@ -71,31 +103,31 @@ class Emulate(WestCommand):
         
         log.inf('Generating ' + str(machine_count) + ' instances')
         for x in range(machine_count):
-            board_mach_name = 'B' + str(x)
+            board_mach_name = platform_name + '_' + str(x)
             # Create a board instance
             board_mach = e.add_mach(board_mach_name)
             
             # Load platform description
-            board_mach.load_repl(output_dir + 'platform.repl')
+            board_mach.load_repl(platform_file)
 
             # If we're debugging, start GDB server. Clients can connect with gdb-multiarch.
             if args.execution == 'debug':
                 board_mach.StartGdbServer(3333)
 
             # Load executable
-            board_mach.load_elf(output_dir + 'zephyr.elf')
+            board_mach.load_elf(bin_dir + 'zephyr.elf')
 
             # Show analyzer output from the emulated board
             if console_node_name:
                 Analyzer(board_mach.sysbus.get_child(console_node_name)).Show()
                     
-                # e.CreateUartPtyTerminal("console_term", "/tmp/" + console_node_name)
+                # e.CreateUartPtyTerminal('console_term', '/tmp/' + console_node_name)
                 # e.Connector.Connect(board_mach.sysbus.get_child(console_node_name).internal, e.externals.console_term)
                     
-                # e.CreateServerSocketTerminal(3456, "console_server")
+                # e.CreateServerSocketTerminal(3456, 'console_server')
                 # e.Connector.Connect(board_mach.sysbus.get_child(console_node_name).internal, e.externals.console_server)
         
-            board_mach.EnableProfiler(output_dir + '/' + board_mach_name + '_Profiler.log')
+            board_mach.EnableProfiler(bin_dir + '/' + board_mach_name + '_Profiler.log')
 
         log.inf('Starting emulation') 
 
@@ -106,7 +138,7 @@ class Emulate(WestCommand):
             # Start all boards and let them run indefinitely
             e.StartAll()
 
-        log.inf('Running emulator. Find run logs for each emulated machine in ' + output_dir +
+        log.inf('Running emulator. Find run logs for each emulated machine in ' + bin_dir +
                 '. Logs cannot be parsed unless emulation is cleanly terminated ' + 
                 '(i.e. don\'t interrupt this session with Ctrl+C. Press ENTER to stop.)')
 

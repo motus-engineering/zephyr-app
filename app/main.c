b0085eff98bd09ca10e5bedd81c509255685523e
MotusDesignSallySchupbach <sally.schupbach@motusdesign.ca>
2024-02-08 16:16:22 -0800

    adding board and support docs

--- app/main.c
+++ app/main.c
@@ -7,13 +7,20 @@
 
 #include <app_version.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/util.h>
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
 {
-    // Log firmware version
-    LOG_INF("Motus Zephyr App Firmware %s", APP_VERSION_STRING);
+    while (1)
+    {
+        // Log firmware version
+        LOG_INF("Motus Zephyr App Firmware %s", APP_VERSION_STRING);
+
+        k_sleep(K_MSEC(1000));
+    }
 
     // Let main complete, Zephyr will handle task switching from here
     return 0;
